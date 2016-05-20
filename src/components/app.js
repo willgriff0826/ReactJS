a48d438dcdfe507ae1659d6c8acb40997eb7e7aa
willgriff0826 <wgriffin038@gmail.com>
2016-05-19 20:46:33 -0500

    setup complete and h3 is displayed on the website

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      	<h3>Welcom to your page!</h3>
+      </div>
+    );
+  }
+}
